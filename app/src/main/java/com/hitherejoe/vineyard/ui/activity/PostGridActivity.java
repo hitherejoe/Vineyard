0d477c961e7fd4af78d33300c0ca72b05e8828da
Joe Birch <hitherejoebirch@googlemail.com>
2015-12-12 21:20:20 +0000

    PostGridActivity error handling
    Show an error fragment when there is an error within the
    PostGridActivity

--- app/src/main/java/com/hitherejoe/vineyard/ui/activity/PostGridActivity.java
+++ app/src/main/java/com/hitherejoe/vineyard/ui/activity/PostGridActivity.java
@@ -1,17 +1,25 @@
 package com.hitherejoe.vineyard.ui.activity;
 
+import android.app.Fragment;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v17.leanback.app.ErrorFragment;
+import android.view.View;
+import android.widget.FrameLayout;
 
 import com.hitherejoe.vineyard.R;
 import com.hitherejoe.vineyard.data.model.Tag;
 import com.hitherejoe.vineyard.data.model.User;
 import com.hitherejoe.vineyard.ui.fragment.PostGridFragment;
 
+import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class PostGridActivity extends BaseActivity {
+
+    @Bind(R.id.frame_container)
+    FrameLayout mFragmentContainer;
 
     public static final String SELECTED_ITEM = "selected_item";
 
@@ -24,14 +32,27 @@ public class PostGridActivity extends BaseActivity {
         Bundle bundle = getIntent().getExtras();
         Object selectedItem = bundle.getParcelable(SELECTED_ITEM);
 
-        if (selectedItem == null) {
-            throw new IllegalArgumentException();
-        } else if (!(selectedItem instanceof Tag)
-                && !(selectedItem instanceof User)) {
-            throw new IllegalArgumentException();
+        Fragment fragment;
+        if (selectedItem == null ||
+                (!(selectedItem instanceof Tag) && !(selectedItem instanceof User))) {
+            fragment = buildErrorFragment();
+        } else {
+            if (selectedItem instanceof Tag) {
+                Tag tag = (Tag) selectedItem;
+                fragment = PostGridFragment.newInstance(PostGridFragment.TYPE_TAG, tag.tag);
+            } else {
+                User user = (User) selectedItem;
+                fragment = PostGridFragment.newInstance(PostGridFragment.TYPE_USER, user.userId);
+            }
         }
-        PostGridFragment mFragment = (PostGridFragment) getFragmentManager().findFragmentById(R.id.fragment_post_grid);
-        mFragment.setTag(selectedItem);
+        getFragmentManager().beginTransaction().replace(mFragmentContainer.getId(), fragment)
+                .addToBackStack(null).commit();
+    }
+
+    @Override
+    public boolean onSearchRequested() {
+        startActivity(new Intent(this, SearchActivity.class));
+        return true;
     }
 
     public static Intent newStartIntent(Context context, Tag selectedTag) {
@@ -46,9 +67,18 @@ public class PostGridActivity extends BaseActivity {
         return intent;
     }
 
-    @Override
-    public boolean onSearchRequested() {
-        startActivity(new Intent(this, SearchActivity.class));
-        return true;
+    private ErrorFragment buildErrorFragment() {
+        ErrorFragment errorFragment = new ErrorFragment();
+        errorFragment.setTitle(getString(R.string.text_error_oops_title));
+        errorFragment.setMessage(getString(R.string.text_error_oops_message));
+        errorFragment.setButtonText(getString(R.string.text_error_dismiss));
+        errorFragment.setButtonClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+        return errorFragment;
     }
+
 }
