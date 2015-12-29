5d3b41ff7e049986370ab8a708519639372719e5
Joe Birch <hitherejoebirch@googlemail.com>
2015-12-29 12:01:35 +0000

    Remove unused import

--- app/src/main/java/com/hitherejoe/vineyard/ui/activity/SearchActivity.java
+++ app/src/main/java/com/hitherejoe/vineyard/ui/activity/SearchActivity.java
@@ -5,7 +5,6 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import com.hitherejoe.vineyard.R;
-import com.hitherejoe.vineyard.ui.fragment.PostGridFragment;
 import com.hitherejoe.vineyard.ui.fragment.SearchFragment;
 
 public class SearchActivity extends BaseActivity {
