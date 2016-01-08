266c0d0e94a53d65cb79f1f0710041ca0b4235f9
Nick <handnf@gmail.com>
2016-01-08 01:24:49 -0500

    Removed ChannelSurfer component, uses a gradle library, broken

--- app/src/main/java/com/hitherejoe/vineyard/livechannels/VineInputProvider.java
+++ app/src/main/java/com/hitherejoe/vineyard/livechannels/VineInputProvider.java
@@ -1,18 +1,29 @@
 package com.hitherejoe.vineyard.livechannels;
 
+import android.content.ComponentName;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.media.MediaPlayer;
+import android.media.tv.TvContract;
 import android.net.Uri;
+import android.os.Bundle;
 import android.util.Log;
 import android.view.Surface;
 import android.view.View;
 
-import channelsurfer.model.Channel;
-import channelsurfer.model.Program;
-import channelsurfer.service.TvInputProvider;
+import com.felkertech.channelsurfer.LibraryUtils;
+import com.felkertech.channelsurfer.model.Channel;
+import com.felkertech.channelsurfer.model.Program;
+import com.felkertech.channelsurfer.service.MediaPlayerInputProvider;
+import com.felkertech.channelsurfer.service.TvInputProvider;
+import com.felkertech.channelsurfer.sync.SyncAdapter;
 import com.hitherejoe.vineyard.data.remote.VineyardService;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 
 import retrofit.Callback;
@@ -22,10 +33,9 @@ import retrofit.Retrofit;
 /**
  * Created by guest1 on 1/6/2016.
  */
-public class VineInputProvider extends TvInputProvider {
-    private MediaPlayer mediaPlayer;
-    private Surface mSurface;
+public class VineInputProvider extends MediaPlayerInputProvider {
     private static final String TAG = "VineInputProvider";
+    private HashMap<Channel, List<Program>> epg;
 
     public VineInputProvider() {}
 
@@ -34,17 +44,17 @@ public class VineInputProvider extends TvInputProvider {
         List<Channel> topics = new ArrayList<>();
         topics.add(new Channel()
                 .setName("Popular Vines")
-                .setServiceId(1)
-                .setOriginalNetworkId(1)
-                .setTransportStreamId(1)
                 .setNumber("101"));
         topics.add(new Channel()
                 .setName("Editor's Vines")
-                .setServiceId(2)
-                .setOriginalNetworkId(2)
-                .setTransportStreamId(2)
                 .setNumber("102"));
         return topics;
+    }
+
+    @Override
+    public void performCustomSync(SyncAdapter syncAdapter, String inputId) {
+
+        super.performCustomSync(syncAdapter, inputId);
     }
 
     @Override
@@ -53,32 +63,15 @@ public class VineInputProvider extends TvInputProvider {
         int SEGMENT = 1000*60*60; //Hour long segments
         List<Program> programList = new ArrayList<>();
         for(int i=0;i<programs;i++) {
-            programList.add(getGenericProgram(channelInfo)
-                    .setStartTimeUtcMillis((startTimeMs + SEGMENT * i))
-                    .setEndTimeUtcMillis((startTimeMs + SEGMENT * (i + 1)))
+            programList.add(new Program.Builder(getGenericProgram(channelInfo))
+                            .setVideoHeight(720)
+                            .setVideoWidth(720)
+                            .setStartTimeUtcMillis((startTimeMs + SEGMENT * i))
+                            .setEndTimeUtcMillis((startTimeMs + SEGMENT * (i + 1)))
+                            .build()
             );
         }
         return programList;
-    }
-
-    @Override
-    public boolean onSetSurface(Surface surface) {
-        mSurface = surface;
-        return true;
-    }
-
-    @Override
-    public void onSetStreamVolume(float volume) {
-        if(mediaPlayer == null)
-            mediaPlayer = new MediaPlayer();
-        mediaPlayer.setVolume(volume, volume);
-    }
-
-    @Override
-    public void onRelease() {
-        mediaPlayer.stop();
-        mediaPlayer.reset();
-        mediaPlayer.release();
     }
 
     @Override
@@ -87,16 +80,19 @@ public class VineInputProvider extends TvInputProvider {
         return null;
     }
 
+    private Channel mChannel;
     @Override
     public boolean onTune(Channel channel) {
         //Check the channels
         //Popular
         notifyVideoUnavailable(REASON_BUFFERING);
         Log.d(TAG, "Tuning to "+channel.getName());
+        Log.d(TAG, "We are playing "+getProgramRightNow(channel));
         if(mediaPlayer == null)
             mediaPlayer = new MediaPlayer();
         VineyardService vineyardService = VineyardService.Creator.newVineyardService();
 
+        mChannel = channel;
         if(channel.getNumber().equals("101")) {
             vineyardService.getPopularPosts().enqueue(new Callback<VineyardService.PostResponse>() {
                 @Override
@@ -122,16 +118,11 @@ public class VineInputProvider extends TvInputProvider {
                 }
             });
         }
-
-
         return true;
     }
 
     public void tuneResponse(Response<VineyardService.PostResponse> response) {
         VineyardService.PostResponse postResponse = response.body();
-        String url = postResponse.data.records.get(0).videoUrl;
-//        Log.d(TAG, "Play " + url);
-        notifyVideoAvailable();
         setOverlayEnabled(false);
         try {
             beginPlayingVine(postResponse, 0);
@@ -151,14 +142,11 @@ public class VineInputProvider extends TvInputProvider {
                 Log.d(TAG, "Release media player");
                 mediaPlayer.stop();
                 mediaPlayer.reset();
-//                mediaPlayer = null;
                 beginPlayingVine(response, index);
                 return;
             } catch(Exception e) {
                 e.printStackTrace();
             }
-//            mSurface = null;
-//            return;
         }
         mediaPlayer.setSurface(mSurface);
         mediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
@@ -176,12 +164,13 @@ public class VineInputProvider extends TvInputProvider {
                             if (index + 1 < response.data.records.size())
                                 beginPlayingVine(response, index + 1);
                             else
-                                beginPlayingVine(response, 0); //Loop to the beginning. Changing channels will reset data (or we can do that ourselves later TODO)
+                                onTune(mChannel);
                         } catch (IOException e) {
                             e.printStackTrace();
                         }
                     }
                 });
+                notifyVideoAvailable();
                 mp.start();
             }
         });
--- channelsurfer/src/main/java/channelsurfer/service/SimpleTvSetup.java
+++
@@ -1,7 +0,0 @@
-package channelsurfer.service;
-
-/**
- * Created by guest1 on 1/6/2016.
- */
-public class SimpleTvSetup extends SetupTvInputProviderActivity {
-}
